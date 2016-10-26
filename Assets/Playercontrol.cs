dc5e557774fba7aa83a86310ff6a80a8c21a0de7
DaaseAllan <laurits.mumberg@gmail.com>
2016-10-26 19:54:37 +0200

    hat

---
+++ Assets/Playercontrol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontrol : MonoBehaviour {
+	private Rigidbody2D PlayerRig;
+	private bool selvmord;
+	// Use this for initialization
+	void Start () {
+		PlayerRig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Movement ();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (selvmord == false) {
+				Movement ();
+			}
+			selvmord = true;
+		}
+		selvmord = false;
+	}
+	private void Movement(){
+		PlayerRig.AddForce(Vector2.up*200);
+	}
+}
